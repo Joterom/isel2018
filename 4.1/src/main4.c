d3d092566a7c3f754e88fb7e6de50be3cca07f34
Joterom <joterom@alumnos.upm.es>
2018-02-20 20:48:38 +0100

    4 y 5 corregidos

--- 4.1/src/main4.c
+++ 4.1/src/main4.c
@@ -5,8 +5,9 @@
 #include "fsm.h"
 #define PERIOD_TICK 100/portTICK_RATE_MS
 #define ETS_GPIO_INTR_ENABLE() _xt_isr_unmask(1 << ETS_GPIO_INUM);
-#define REBOUND_TICK 150
-volatile int rebote;
+#define REBOUND_TICK 50 //500ms -> TOTALMENTE ARBITRARIO, un tiempo grande era mas facil de testear
+volatile int boton0;
+volatile int boton15;
 enum fsm_state {
   LED_ON,
   LED_OFF,
@@ -56,43 +57,53 @@ uint32 user_rf_cal_sector_set(void)
 }
 
 int button_pressed (fsm_t* self){
-  //printf("\nLlamada a button_pressed: \nREBOTE = %i\nPULSADOR 0: %i\nPULSADOR 15: %i\n",rebote,!GPIO_INPUT_GET(0),GPIO_INPUT_GET(15));
-  //if (rebote = 0){
-    if (!GPIO_INPUT_GET(0)){
+    if (boton0){
       printf("GPIO 0 pulsado -->");
     }
-    if (GPIO_INPUT_GET(15)){
+    if (boton15){
       printf("GPIO 15 pulsado -->");
     }
-    return (GPIO_INPUT_GET(15) || !GPIO_INPUT_GET(0));
+    return (boton0 || boton15);
 }
 
 void led_on (fsm_t* self) {
+  boton0 = 0;
+  boton15 = 0;
   printf("Estoy encendido\n");
   GPIO_OUTPUT_SET(2, 0);
 }
 void led_off (fsm_t* self) {
+  boton0 = 0;
+  boton15 = 0;
   printf("Estoy apagado\n");
   GPIO_OUTPUT_SET(2, 1);
 }
 
 void isr_gpio(void* arg){
   uint32 status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
-  static portTickType ultimotick = 0;
+  static portTickType ultimotick0 = 0;
+  static portTickType ultimotick15 = 0;
   portTickType ahora = xTaskGetTickCount();
-  //printf("\nSe atiende la interrupcion: valor de rebote = %i",rebote);
-  //printf("\nTiempo actual: %i",ahora);
-  if (status){
-    if (ahora > ultimotick){
-      ultimotick = ahora + REBOUND_TICK;
-      //rebote = 0;
-      //printf("\nNo se produce rebote: valor de rebote = %i\nSaliendo\n",rebote);
+
+  if (status & BIT(0)){
+    if (ahora > ultimotick0){
+      ultimotick0 = ahora + REBOUND_TICK;
+      boton0 = 1;
+      printf("\nPulsacion detectada, GPIO0\nTiempo actual : %i\nTiempo de apagado: %i\n",ahora,ultimotick0);
     } else {
-      //rebote = 1;
-      //printf("\nSe produce rebote: valor de rebote = %i\nSaliendo\n",rebote);
+      printf("\nAntirrebotes lanzado, GPIO0\nTiempo actual: %i\n",ahora);
     }
   }
 
+  if (status & BIT(15)){
+    if (ahora > ultimotick15){
+      ultimotick15 = ahora + REBOUND_TICK;
+      boton15 = 1;
+      printf("\nPulsacion detectada, GPIO15\nTiempo actual : %i\nTiempo de apagado: %i\n",ahora,ultimotick15);
+    } else {
+      printf("\nAntirrebotes lanzado, GPIO15\nTiempo actual: %i\n",ahora);
+    }
+  }
   GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, status);
 }
 
@@ -102,8 +113,8 @@ void inter(void* ignore){
   PIN_FUNC_SELECT(GPIO_PIN_REG_0, FUNC_GPIO0);
   GPIO_AS_OUTPUT(2);
   static fsm_trans_t interruptor[]={
-    {LED_ON, button_pressed, LED_OFF, led_off },
     {LED_OFF, button_pressed, LED_ON, led_on},
+    {LED_ON, button_pressed, LED_OFF, led_off },
     {-1, NULL, -1, NULL },
   };
 
--- 5.1/src/main5.c
+++ 5.1/src/main5.c
@@ -5,9 +5,11 @@
 #include "fsm.h"
 #define PERIOD_TICK 100/portTICK_RATE_MS
 #define ETS_GPIO_INTR_ENABLE() _xt_isr_unmask(1 << ETS_GPIO_INUM);
-#define REBOUND_TICK 150
+#define REBOUND_TICK 50 //Mismo comentario que en el ejercicio anterior
 #define TIEMPO_ON 6000
 volatile portTickType tiempo_de_apagado;
+volatile int boton0;
+volatile int boton15;
 enum fsm_state {
   LED_ON,
   LED_OFF,
@@ -58,56 +60,63 @@ uint32 user_rf_cal_sector_set(void)
 
 int led_timeout (fsm_t* self) {
    portTickType now = xTaskGetTickCount();
-   //printf("\nPARAMETROS TIMEOUT: \nValor de now: %i\nValor de tiempo_apagado: %i\n",now,tiempo_de_apagado);
    if (now >= tiempo_de_apagado){
      return(1);
-   } else {
-     return(0);
    }
+   return(0);
 }
 
 int button_pressed (fsm_t* self){
-  //printf("\nLlamada a button_pressed: \nREBOTE = %i\nPULSADOR 0: %i\nPULSADOR 15: %i\n",rebote,!GPIO_INPUT_GET(0),GPIO_INPUT_GET(15));
-  //if (rebote = 0){
-    if (!GPIO_INPUT_GET(0)){
-      printf("GPIO 0 pulsado --> ");
+    if (boton0){
       tiempo_de_apagado = xTaskGetTickCount() + TIEMPO_ON;
-    } else if (GPIO_INPUT_GET(15)){
-      printf("GPIO 15 pulsado --> ");
-      tiempo_de_apagado = xTaskGetTickCount() + TIEMPO_ON;
+      printf("GPIO 0 pulsado -->");
     }
-
-
-    printf("\nSe apagara en: %i\n",tiempo_de_apagado);
-    return (GPIO_INPUT_GET(15) || !GPIO_INPUT_GET(0));
+    if (boton15){
+      tiempo_de_apagado = xTaskGetTickCount() + TIEMPO_ON;
+      printf("GPIO 15 pulsado -->");
+    }
+    return (boton0 || boton15);
 }
 
 void led_on (fsm_t* self) {
+  boton0 = 0;
+  boton15 = 0;
   printf("Estoy encendido\n");
   GPIO_OUTPUT_SET(2, 0);
 }
+
 void led_off (fsm_t* self) {
+  boton0 = 0;
+  boton15 = 0;
   printf("Estoy apagado\n");
   GPIO_OUTPUT_SET(2, 1);
 }
 
 void isr_gpio(void* arg){
   uint32 status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
-  static portTickType ultimotick = 0;
+  static portTickType ultimotick0 = 0;
+  static portTickType ultimotick15 = 0;
   portTickType ahora = xTaskGetTickCount();
-  //printf("\nSe atiende la interrupcion: valor de rebote = %i",rebote);
-  //printf("\nTiempo actual: %i",ahora);
-  if (status){
-    if (ahora > ultimotick){
-      ultimotick = ahora + REBOUND_TICK;
-      //rebote = 0;
-      //printf("\nNo se produce rebote: valor de rebote = %i\nSaliendo\n",rebote);
+
+  if (status & BIT(0)){
+    if (ahora > ultimotick0){
+      ultimotick0 = ahora + REBOUND_TICK;
+      boton0 = 1;
+      printf("\nPulsacion detectada, GPIO0\nTiempo actual : %i\nTiempo de rebote: %i\n",ahora,ultimotick0);
     } else {
-      //rebote = 1;
-      //printf("\nSe produce rebote: valor de rebote = %i\nSaliendo\n",rebote);
+      printf("\nAntirrebotes lanzado, GPIO0\nTiempo actual: %i\n",ahora);
     }
   }
 
+  if (status & BIT(15)){
+    if (ahora > ultimotick15){
+      ultimotick15 = ahora + REBOUND_TICK;
+      boton15 = 1;
+      printf("\nPulsacion detectada, GPIO15\nTiempo actual : %i\nTiempo de rebote: %i\n",ahora,ultimotick15);
+    } else {
+      printf("\nAntirrebotes lanzado, GPIO15\nTiempo actual: %i\n",ahora);
+    }
+  }
   GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, status);
 }
 
@@ -117,8 +126,8 @@ void inter(void* ignore){
   PIN_FUNC_SELECT(GPIO_PIN_REG_0, FUNC_GPIO0);
   GPIO_AS_OUTPUT(2);
   static fsm_trans_t interruptor[]={
-    {LED_ON, led_timeout, LED_OFF, led_off },
     {LED_OFF, button_pressed, LED_ON, led_on},
+    {LED_ON, led_timeout, LED_OFF, led_off },
     {-1, NULL, -1, NULL },
   };
 
